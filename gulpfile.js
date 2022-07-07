332212e9180981afc8b28a3db721b984bf48845d
Saynick <oscarmartinezsalazar@gmail.com>
2022-07-07 17:27:44 -0500

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
-function tarea (){
-    console.log('primera tarea');
+const {src ,dest, watch} = require("gulp");
+const sass =require ("gulp-sass")(require('sass'));
+
+function css (done){
+     src('src/scss/app.scss')//Identificar el archivo de SASS
+    .pipe(sass())//Compilar las funciones de SASS
+    .pipe(dest("build/css"));//Almacenarla en el disco duro
+
+    done();//callback que avisa a gulp cuando llegamos al final
 }
 
-exports.primerTarea = tarea;
+function dev(done){
+    watch('src/scss/app.scss', css)
+    done ();
+}
+
+exports.css = css; //npm run dev "O" npx gulp dev// esto será a manera de ejecutar cualquiera de las ambas opciones
+exports.dev = dev;
